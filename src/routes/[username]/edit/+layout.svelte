3026b76c1a81a8f9c7d6cf748906ecfb7297061d
Kyle Malloy <kmalloy24@gmail.com>
2023-09-05 14:32:52 -0400

    edit

--- src/routes/[username]/edit/+layout.svelte
+++ src/routes/[username]/edit/+layout.svelte
@@ -5,14 +5,6 @@
     $userData;
   </script>
 
-<!-- <details class="dropdown mb-32">
-    <summary class="m-1 btn">open or close</summary>
-    <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-      <li><a>Item 1</a></li>
-      <li><a>Item 2</a></li>
-    </ul>
-  </details>
-
   <div class="min-h-screen flex flex-col">
     <slot />
-  </div> -->
+  </div>
